e1b11bea5578564b6f54de22554a471532c6370a
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-09-14 13:00:14 +0900

    Update

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        "assets/images/Logo.svg",
+                        width: 50,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset("assets/images/Menu.svg"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Image.asset("assets/images/Web3.jpg"),
+                const SizedBox(height: 20),
+                const Text(
+                  "The Bright Future of Web 3.0?",
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontVariations: [
+                      FontVariation("wght", 900),
+                    ],
+                    height: 1,
+                    color: Colors.black87,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
