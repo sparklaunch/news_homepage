1e10754e59f8444b77c0e1a7d2e85e1f12b7db64
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-09-14 13:07:26 +0900

    Update main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -12,7 +12,7 @@ class MainScreen extends StatelessWidget {
           padding: const EdgeInsets.all(20),
           child: SingleChildScrollView(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -43,6 +43,34 @@ class MainScreen extends StatelessWidget {
                     height: 1,
                     color: Colors.black87,
                   ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black87,
+                    height: 1.6,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 40,
+                    ),
+                    shape: const RoundedRectangleBorder(),
+                    backgroundColor: const Color.fromRGBO(240, 82, 71, 1),
+                    foregroundColor: const Color.fromRGBO(0, 0, 25, 1),
+                    textStyle: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 3,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text("READ MORE"),
                 ),
               ],
             ),
